403e20dbe01132babada176b15d1bc851dcac3c1
yoonwooseong <57824259+yoonwooseong@users.noreply.github.com>
2020-01-11 19:59:17 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 
 canvas.width = 700;
-canvas.heighy = 700;
+canvas.height = 700;
 
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
